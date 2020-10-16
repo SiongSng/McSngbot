5eabc22bff8fe8fdcf147625b437a9c6c5be8001
rrt467778 <48402225+rrt467778@users.noreply.github.com>
2020-10-16 22:57:26 +0800

    Add files via upload

--- discord.js
+++ discord.js
@@ -1,49 +1,21 @@
-/*
- * This example is an a very simple way how to connect a discord bot with a mineflayer bot.
- * For this example you will need discord.js installed. You can install with: npm install discord.js
- * Note that discord.js v12 or newer is required.
- * You need to do this before running this example:
- * - You need a discord token
- * - You need to get the id of the channel you want to use
- */
-
-if (process.argv.length < 6 || process.argv.length > 8) {
-  console.log('Usage : node discord.js <discord bot token> <channel id> <host> <port> [<name>] [<password>]')
-  process.exit(1)
-}
-
+const config = require(`${process.cwd()}/config.json`)
 // Load discord
 const Discord = require('discord.js')
 const client = new Discord.Client()
-let channel = process.argv[3]
-
+// 1234
+client.login('請輸入機器人token')
 // Load mineflayer
 const mineflayer = require('mineflayer')
 const bot = mineflayer.createBot({
-  host: process.argv[4],
-  port: parseInt(process.argv[5]),
-  username: process.argv[6] || 'discord',
-  password: process.argv[7]
+  host: 'mcfallout.net', 
+    port:25565, 
+    username: config.username,
+    password: config.password,
 })
-
-client.on('ready', () => {
-  console.log(`The discord bot logged in! Username: ${client.user.username}!`)
-  channel = client.channels.cache.get(channel)
-  if (!channel) {
-    console.log(`I could not find the channel (${process.argv[3]})!\nUsage : node discord.js <discord bot token> <channel id> <host> <port> [<name>] [<password>]`)
-    process.exit(1)
-  }
+bot.once('spawn', () => {
+    const channel = client.channels.cache.find(channel => channel.id ===`請輸入頻道ID`)
+    console.log('Minecraft 殭屍豬人塔Bot 啟動成功 請勿關閉此視窗')
+   setInterval(function () {
+       channel.send(`${bot.username}目前經驗等級為${bot.experience.level}級`);
+   }, 60000)
 })
-
-//經驗值等級公告
-bot.on('chat', (username, message,) => {
-  // Ignore messages from the bot itself
-  if (username === bot.username) return
-  channel.send(`${bot.username}目前經驗等級為${bot.experience.level}級`)
-})
-
-// Login Discord bot
-client.login(process.argv[2])
-
-
-
