8f4ee8e09032cc6270361bec786a9b7f8f5655f0
菘菘 <rrt467778@gmail.com>
2021-01-01 09:28:53 +0800

    修正 #7 新增自動打怪支援怪物

--- McSngbot.js
+++ McSngbot.js
@@ -1,23 +1,12 @@
-//version 1.1.3
+//version 1.1.4
 const config = require(`${process.cwd()}/config.json`)  //讀取config(組態)
 const settings = require(`${process.cwd()}/settings.json`) //讀取設定檔案
-const fs = require('fs'); //讀取fs模塊
 const tokens = require('prismarine-tokens-fixed');  //讀取prismarine-tokens-fixed(驗證緩存)模塊
 const mineflayer = require('mineflayer');  //讀取mineflayer模塊
 
 //取得時間
 const sd = require('silly-datetime'); //讀取silly-datetime模塊
 const time =sd.format(new Date(), 'YYYY-MM-DD HH-mm-ss'); //獲得系統時間
-fs.mkdir('./logs', { recursive: true }, (err) => {
-    if (err) throw err;
-});
-
-fs.writeFile(`./logs/log-${time}.txt`, '【McSngbot】bot載入中...',"utf8", function (err) {
-    if (err)
-        console.log(`錯誤: ${err}`);
-    else
-        console.log(`【McSngbot】bot載入中...`);
-});
 
 let loginOpts = {  //登入資訊
     host: config.ip,  //伺服器ip
@@ -29,23 +18,19 @@ let loginOpts = {  //登入資訊
     version: "1.16.4"  //bot的Minecraft版本
 }
 
-
 function connects() {
     tokens.use(loginOpts, function (_err, _opts) { //使用驗證緩存
         const bot = mineflayer.createBot(_opts) //定義bot為mineflayer類別中的createBot
 
         bot.once('spawn', () => {   //bot啟動時
-            console.log(`bot載入完成\n-------------------------------------`)
+            console.log(`McSngbot載入完成\n`)
             //自動宣傳
-            if (settings.Publicity === `true`) {
+            if (settings.Publicity === true) {
                 setInterval(function () {
-                    bot.chat(`${settings.Pycontent}`)
+                    bot.chat(`${settings.Pont}`)
                 }, 21600000)
                 setInterval(function () {
-                    bot.chat(`${settings.Toncontent} .w.`)
-                }, 21600000)
-                setInterval(function () {
-                    bot.chat(`${settings.Transaction}`)
+                    bot.chat(`${settings.Tont}`)
                 }, 600000)
             }
             //從小黑窗中發送訊息
@@ -56,43 +41,36 @@ function connects() {
                 terminal: false
             });
             rl.on('line', function (line) {
-                let data = `\n[cmd] ${line}`
-                require("./function/logs")(fs, data, time);
                 bot.chat(line)
             })
             // 讀取設定檔案並且開始打怪
-            if (settings.attack === `true`) {
+            if (settings.attack === true) {
                 require("./commands/attack")(bot);
-                require("./commands/Discardeditems")(bot);
+                require("./Utility/Discardeditems")(bot);
             }
         });
         bot.on("message", async function (jsonMsg) {
-            const health = /目標生命 \: ❤❤❤❤❤❤❤❤❤❤ \/ ([\S]+)/g.exec(jsonMsg.toString()) //清除目標生命
-            if (settings.health === `false`) {
+            let health = /目標生命 \: ❤❤❤❤❤❤❤❤❤❤ \/ ([\S]+)/g.exec(jsonMsg.toString()); //清除目標生命
+            if (settings.health === false) {
                 if (health) {
                     return;
                 } else {
-                    let data = `\n${jsonMsg}`
-                    await require("./function/logs")(fs, data, time);
-                    console.log(jsonMsg.toAnsi())
+                    console.log(jsonMsg.toAnsi());
                 }
             }
-            if (settings.health === `true`) {
-                let data = `\n${jsonMsg}`
-                await require("./function/logs")(fs, data, time);
-                console.log(jsonMsg.toAnsi())
+            if (settings.health === true) {
+                console.log(jsonMsg.toAnsi());
             }
         })
         const whitelist = (config.whitelist)  //定義白名單
-        const whitelist2 = "Barry23412"  //默認我為白名單(Barry23412,菘菘)
         //自動接受或拒絕/tpa /tpahere
         bot.on("message", async function (jsonMsg) {
             if (jsonMsg.toString().startsWith(`[廢土伺服] :`) &&
                 jsonMsg.toString().toLowerCase().includes(`想要你傳送到 該玩家 的位置!`) ||
                 jsonMsg.toString().toLowerCase().includes(`想要傳送到 你 的位置`)) {
-                let dec = jsonMsg.toString().split(/ +/g);
-                let playerid = dec[2] //2
-                if (whitelist.includes(playerid) || whitelist2.includes(playerid)) {
+                let msg = jsonMsg.toString().split(/ +/g);
+                let playerid = msg[2]
+                if (whitelist.includes(playerid)){
                     bot.chat(`/tok`)
                 } else {
                     bot.chat(`/tno`)
@@ -105,15 +83,14 @@ function connects() {
                 let lo = dec[2].split(`]`)//
                 let playerid = dec.splice(lo.length)[0].split("]") //取得Minecraft ID
                 let args = msg.slice(10 + playerid[0].length).split(" ")  //取得指令內容
-                if (whitelist.includes(`${playerid[0]}`) ||
-                    (whitelist2.includes(`${playerid[0]}`))) {
+                if (whitelist.includes(`${playerid[0]}`)) {
                     switch (args[0]) { //指令前綴
                         case "attack":
-                            if (settings.attack === `true`) {
-                                bot.chat(`/m ${playerid[0]} 已成功開啟自動打怪功能 (/m botID attack-help 顯示更多資訊`)
-                            } else if (settings.attack === `false`) {
-                                bot.chat(`/m ${playerid[0]} 已成功開啟自動打怪功能 (/m botID attack-help 顯示更多資訊`)
-                                await require("./commands/Discardeditems")(bot);
+                            if (settings.attack === true) {
+                                bot.chat(`/m ${playerid[0]} 已開啟自動打怪功能`)
+                            } else if (settings.attack === false) {
+                                bot.chat(`/m ${playerid[0]} 已開啟自動打怪功能`)
+                                await require("./Utility/Discardeditems")(bot);
                                 await require("./commands/attack")(bot);
                             }
                             break
@@ -124,9 +101,15 @@ function connects() {
                         case "about":
                             bot.chat(`/m ${playerid[0]} 此bot由菘菘編譯而成，想查看bot相關的教學或想要下載     歡迎加入我的Discord伺服器 (https://discord.gg/5w9BUM4) 點連結即可前往`)
                             break
+                        case "reload":
+                            bot.chat(`/m ${playerid[0]} bot將於3秒後重新啟動...`);
+                            setTimeout(function (){
+                                bot.end();
+                            },3000)
+                            break
                         case "exit":
                             bot.chat(`/m ${playerid[0]} 正在關閉bot中...`)
-                            console.log(`5秒後關閉bot...`)
+                            console.log(`5秒後將關閉bot...`)
                             setTimeout(function () {
                                 process.exit()
                             },5000)
@@ -135,7 +118,7 @@ function connects() {
                             for (let i = 9; i <= 46; i++) {
                                 bot._client.write("window_click", {
                                     windowId: 0,
-                                    slot: i,
+                                    slot: `${i}`, //物品欄
                                     mouseButton: 1,
                                     action: 1,
                                     mode: 4,
@@ -149,7 +132,7 @@ function connects() {
                             break
                         case "throw":  //丟棄指定物品
                             if (args[1] !== undefined) {
-                                if (!isNaN(args[1])) {
+                                if (!isNaN(Number(args[1]))) {
                                     if (args[1] >= 9) {
                                         if (args[1] <= 46) {
                                             bot._client.write("window_click", {
@@ -165,30 +148,26 @@ function connects() {
                                         }
                                     }
                                 } else {
-                                    bot.chat(`/m ${playerid[0]} 請輸入有效數字 而並非 ${msg[2]}`)
+                                    bot.chat(`/m ${playerid[0]} 請輸入有效數字 而不是 ${msg[2]}`)
                                     return;
                                 }
                             }
                             break
                     }
                 } else {
-                    bot.chat(`/m ${playerid[0]} [${playerid[0]}] 不在【McSngbot】指令白名單內 (請確認白名單是否正確或者私訊菘菘#8663)`)
+                    bot.chat("/m" + playerid[0]+ "您不在【McSngbot】指令白名單內 如有問題，請私訊Discord(菘菘#8663)")
                 }
             }
         })
         //kick 被伺服器踢出事件
-        bot.once('kicked', (reason, loggedIn) => {
+        bot.once('kicked', (reason) => {
             let time1 =sd.format(new Date(), 'YYYY-MM-DD HH-mm-ss'); //獲得系統時間
-            let data = `【McSngbot】[資訊] 客戶端與伺服器斷線\n斷線時間:${time1}   原因 :\n${reason}`
-            require("./function/logs")(fs, data, time);
-            console.log(data)
+            console.log(`[資訊] 客戶端被伺服器踢出 @${time1}   原因 :\n${reason}`)
         });
         //斷線自動重連
         bot.once('end', () => {
             let time1 =sd.format(new Date(), 'YYYY-MM-DD HH-mm-ss'); //獲得系統時間
-            let data =`【McSngbot】[資訊] 客戶端與伺服器斷線 ，5秒後將會自動重新連線...\n斷線時間:${time1}`
-            console.log(data)
-            require("./function/logs")(fs, data, time);
+            console.log(`[資訊] 客戶端與伺服器斷線 ，5秒後將會自動重新連線...\n@${time1}`)
             setTimeout(function () {
                 connects();
             }, 5000)
--- commands/attack.js
+++ commands/attack.js
@@ -1,14 +1,21 @@
 module.exports = async (bot) => {
-    setInterval(function () {
-      for (let mobstoattack in bot.entities) {
-            if (bot.entities[mobstoattack].entityType === 22 ||
-                (bot.entities[mobstoattack].entityType === 62) ||
-                (bot.entities[mobstoattack].entityType === 92) ||
-                (bot.entities[mobstoattack].entityType === 94) ||
-                (bot.entities[mobstoattack].entityType === 96) ||
-                (bot.entities[mobstoattack].entityType === 98)) {
-                bot.attack(bot.entities[mobstoattack])
+    setInterval(async function () {
+        let list = [6, 13, 22, 62, 73, 92, 94, 96, 98, 102]
+        for (let mobstoattack in bot.entities) {
+            if (list.includes(bot.entities[mobstoattack].entityType)) {
+                await bot.attack(bot.entities[mobstoattack])
             }
-            }
+        }
     }, 650)
 }
+/* id所代表的生物:
+ 6  => blaze(烈焰使者)
+ 13 => creeper(苦力怕)
+ 22 => evoker(喚魔者)
+ 62 => pillager(掠奪者)
+ 73 => skeleton(骷髏)
+ 92 => Vex(腦鬼)
+ 94 => villager(衛道士)
+ 96 => witch(女巫)
+ 98 => wither_skeleton(凋零骷髏)
+ 102 => zombie(殭屍) */
